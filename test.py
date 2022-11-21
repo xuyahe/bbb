b86e1bfc978a43e2b8d478b32837952b590dfe8b
tom <haha.com>
2022-11-21 09:47:13 -0500

    aaa

--- test.py
+++ test.py
@@ -1,6 +1,3 @@
-# add xu
-# new add 20221121
-# aaaa
 import yagmail
 yag = yagmail.SMTP(user='xuyahe@126.com', password='xuyahe123', host='smtp.126.com')
 yag.send(['337467239@qq.com'],'this is a mail from xuyahe', 'hello world')
